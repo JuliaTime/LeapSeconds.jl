a14d7d335f9c4f6bbfd1d6544fe54708d1f00f48
Helge Eichhorn <git@helgeeichhorn.de>
2020-03-08 21:50:24 +0100

    Enable docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [LeapSeconds]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "https://github.com/JuliaTime/LeapSeconds.jl.git"
+)
